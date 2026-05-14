23dabfdf209008bc6925b2f6e18aa9b2fde2b97d
codewithria89 <anjalik0822@gmail.com>
2026-05-14 18:20:17 +0530

    Practice of Jagged file

---
+++ src/DataStructure/Array/po2.java
@@ -0,0 +1,29 @@
+package DataStructure.Array;
+
+import java.util.Scanner;
+
+public class po2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a[][] = new int[5][];
+        a[0] = new int[2];
+        a[1]= new int[1];
+        a[2]= new int[5];
+        a[3] = new int[4];
+        a[4] = new int[3];
+        System.out.println("Enter the elements");
+
+        for (int i = 0; i <= a.length - 1; i++) {
+            for (int j = 0; j <= a[i].length - 1; j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("Array elements are: ");
+        for (int i = 0; i <= a.length - 1; i++) {
+            for (int j = 0; j <= a[i].length - 1; j++) {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
