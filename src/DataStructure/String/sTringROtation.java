de040a6456cdc0e4b48884c3cdd4142970d04c40
codewithria89 <anjalik0822@gmail.com>
2026-06-03 18:05:03 +0530

    String rotational prgm

---
+++ src/DataStructure/String/sTringROtation.java
@@ -0,0 +1,40 @@
+package DataStructure.String;
+
+import java.util.Scanner;
+
+public class sTringROtation {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter first string: ");
+            String str1 = scanner.next();
+            System.out.print("Enter second string: ");
+            String str2 = scanner.next();
+            if (str1.length() != str2.length())
+            {
+                System.out.println("The string \"" + str2 + "\" is not a rotation of \"" + str1 + "\"");
+            }
+            else
+
+            {
+
+                String combinedStr = str1 + str1;
+
+                if (combinedStr.contains(str2)) {
+
+
+
+                    System.out.println("The string \"" + str2 + "\" is a rotation of \"" + str1 + "\"");
+
+                } else {
+
+                    System.out.println("The string \"" + str2 + "\" is not a rotation of \"" + str1 + "\"");
+
+                }
+
+            }
+
+            scanner.close();
+
+        }
+
+    }
