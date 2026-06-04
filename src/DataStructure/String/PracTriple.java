d6c7b1a8d60d75131c0f098da0de0e0518daed6c
codewithria89 <anjalik0822@gmail.com>
2026-06-04 17:12:48 +0530

    Triple String Computations

---
+++ src/DataStructure/String/PracTriple.java
@@ -0,0 +1,13 @@
+package DataStructure.String;
+
+import java.util.Scanner;
+
+public class PracTriple {
+    Scanner sc = new Scanner(System.in);
+
+    String str1 = sc.nextLine();
+    String str2 = sc.nextLine();
+    String str3 = sc.nextLine();
+
+    for (int i = str1; i < str1.le)
+}
