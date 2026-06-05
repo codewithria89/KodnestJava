3107c5ef5aea5d9575a61adfb7ead918f78cf087
codewithria89 <anjalik0822@gmail.com>
2026-06-05 18:19:34 +0530

    Collection methods with for Loop and Advance for Loop

---
+++ src/CollectionFramework/Demo3.java
@@ -0,0 +1,24 @@
+package CollectionFramework;
+import java.util.ArrayList;
+
+public class Demo3 {
+    public static void main(String[] args) {
+        ArrayList al = new ArrayList();
+        al.add(10);
+        al.add(20);
+        al.add(30);
+        al.add(40);
+        al.add(50);
+        System.out.println(al);
+        System.out.println("FOR LOOP");
+        for(int i=0 ;i<=al.size()-1; i++)
+        {
+            System.out.println(al.get(i));
+        }
+        System.out.println("ADV FOR LOOP");
+        for(Object x : al)
+        {
+            System.out.println(x);
+        }
+    }
+}
