2867dd72fb6c635b160127c373c0c35f3eb237d9
codewithria89 <anjalik0822@gmail.com>
2026-06-05 17:50:21 +0530

    Adding Collection Framework

---
+++ src/CollectionFramework/Demo1.java
@@ -0,0 +1,20 @@
+package CollectionFramework;
+
+import java.util.ArrayList;
+
+public class Demo1 {
+     public static void main(String[] args) {
+
+         ArrayList al = new ArrayList<>();
+         al.add(10);
+         al.add(20);
+         al.add(30);
+         al.add(40);
+         al.add(50);
+         al.add(60);
+         al.add(70);
+
+         System.out.println(al.size());
+         System.out.println(al);
+    }
+}
