c4839957c94cb4f16158d7f6fc042103c41e244f
codewithria89 <anjalik0822@gmail.com>
2026-06-08 18:06:44 +0530

    Removing Method in ArrayList

---
+++ src/CollectionFramework/removing.java
@@ -0,0 +1,27 @@
+package CollectionFramework;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class removing {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            // Read the number of elements
+            int n = sc.nextInt();
+            // Create an ArrayList to store integers
+            ArrayList<Integer> list = new ArrayList<>();
+            // Add elements to the ArrayList
+            for (int i = 0; i < n; i++) {
+                list.add(sc.nextInt());
+            }
+            // Read index and remove the element
+            int index = sc.nextInt();
+            list.remove(index);
+            // Print the updated ArrayList
+            System.out.println("Updated ArrayList:");
+            for (int num : list) {
+                System.out.print(num + " ");
+            }
+            sc.close();
+        }
+    }
